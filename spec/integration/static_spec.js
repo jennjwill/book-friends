064bb7ac2bfd8b2dcf7a3119f15dfa20961f06cf
Jenn J Will <jennijwill@gmail.com>
2019-10-09 11:41:25 -0700

    coded route, controller, basic view, spec for landing page

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,15 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+  describe("GET /", () => {
+    it("should return status code 200 and have 'Welcome to Book Friends' in the response body", done => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Book Friends");
+        done();
+      });
+    });
+  });
+});
